2a80d04d01b64dab36e7a9182c8f7082994d811d
201211289/유성민 <butter647@hanmail.net>
2017-05-23 13:09:33 +0900

    Add files via upload

--- src/ds_spark_overview.py
+++ src/ds_spark_overview.py
@@ -1,7 +1,16 @@
 #coding:utf-8 
-import pyspark 
+
 import os 
 import matplotlib.pyplot as plt 
+import sys
+os.environ["SPARK_HOME"]="C:\users\Admin\Code\s-201211289\spark-2.0.0-bin-hadoop2.7\spark-2.0.0-bin-hadoop2.7"
+os.environ["PYLIB"]=os.path.join(os.environ["SPARK_HOME"],'python','lib')
+print os.environ["PYLIB"]
+sys.path.insert(0,os.path.join(os.environ["PYLIB"],'py4j-0.10.1-src.zip'))
+sys.path.insert(0,os.path.join(os.environ["PYLIB"],'pyspark.zip'))
+
+import pyspark
+
 myConf      = pyspark.SparkConf() 
 spark       = pyspark.sql.SparkSession.builder.master('local').appName('myApp').config(conf=myConf).getOrCreate() 
 filepath    = os.path.join('data','ds_spark_overview.txt') 
